37bd288ff5f026d811aa9d220daec89a0e01733d
Yogesh Baviskar <yogeshbaviskar0603@gmail.com>
2026-09-15 14:02:10 +0530

    router issue solve

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./pages/login";
 import Register from "./pages/register";
@@ -13,7 +13,7 @@ function App() {
     <BrowserRouter>
 
       <Routes>
-
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route
           path="/login"
           element={<Login />}
